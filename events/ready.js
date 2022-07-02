186e5b7ca8b457ac1971906e8e88272b3e4c80e7
novabotlist <93944142+moderasyonbo@users.noreply.github.com>
2022-07-02 23:21:20 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const config = require('../config.js');
+module.exports = async client => {
+}
+
+// Type kısımları:
+// WATCHING - İZLİYOR
+// PLAYING - OYNUYOR
+// LISTENING - DİNLİYOR
+
+// Status kısımları:
+// online - çevrim içi
+// idle - boşta
+// dnd - rahatsız etmeyin
+
+// name kısmına oynuyorunuzu yazabilirsiniz.
