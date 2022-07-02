1316ee068aae7c7f29053c78fbfc26034afbb79c
novabotlist <93944142+moderasyonbo@users.noreply.github.com>
2022-07-02 23:19:03 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "token": "OTY3NzgzMjg5NjYwMjY0NDk4.GxTn6_.FK7eFA39IiTh50rjcDJMyqhAZoWd7JBlBTxjAM",
+  "geliştiriciler": ["Kendi Discord ID'nizi buraya yazın."], 
+  "prefix": "!"
+};
